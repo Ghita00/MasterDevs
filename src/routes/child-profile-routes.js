6032c8edc3a88a5f336cfbeaa5350378b53adbe7
MattiaSaran <82222954+MattiaSaran@users.noreply.github.com>
2021-11-23 17:19:22 +0100

    Inserimento funzioni di modifica dei parametri riferiti ai diritti

--- src/routes/child-profile-routes.js
+++ src/routes/child-profile-routes.js
@@ -4,12 +4,12 @@ const router = new express.Router()
 const ChildProfile = require('../models/childProfile')
 
 router.get('/', (req, res, next) => {
-  if (!req.user_id) { return res.status(401).send('Not authenticated') }
+  if (!req.child_user_id) { return res.status(401).send('Not authenticated') }
   const { ids } = req.query
   if (!ids) {
     return res.status(400).send('Bad Request')
   }
-  ChildProfile.find({ child_id: { $in: ids } })
+  ChildProfile.find({ child_user_id: { $in: ids } })
     .select('given_name family_name image_id child_id suspended')
     .populate('image')
     .lean()
@@ -19,6 +19,66 @@ router.get('/', (req, res, next) => {
         return res.status(404).send('Children not found')
       }
       res.json(profiles)
+    }).catch(next)
+})
+
+router.get('/rights', (req, res, next) => {
+  if (!req.activity) { return res.status(401).send('Not authenticated') }
+  const { ids } = req.query
+  if (!ids) {
+    return res.status(400).send('Bad Request')
+  }
+  ChildProfile.find({ activity: { $in: ids } })
+    .select('activity')
+    .lean()
+    .exec()
+    .then(child => {
+      child.activity = !child.activity
+    }).catch(next)
+})
+
+router.get('/rights', (req, res, next) => {
+  if (!req.chat) { return res.status(401).send('Not authenticated') }
+  const { ids } = req.query
+  if (!ids) {
+    return res.status(400).send('Bad Request')
+  }
+  ChildProfile.find({ chat: { $in: ids } })
+    .select('chat')
+    .lean()
+    .exec()
+    .then(child => {
+      child.chat = !child.chat
+    }).catch(next)
+})
+
+router.get('/rights', (req, res, next) => {
+  if (!req.partecipation) { return res.status(401).send('Not authenticated') }
+  const { ids } = req.query
+  if (!ids) {
+    return res.status(400).send('Bad Request')
+  }
+  ChildProfile.find({ partecipation: { $in: ids } })
+    .select('partecipation')
+    .lean()
+    .exec()
+    .then(child => {
+      child.partecipation = !child.partecipation
+    }).catch(next)
+})
+
+router.get('/rights', (req, res, next) => {
+  if (!req.manage) { return res.status(401).send('Not authenticated') }
+  const { ids } = req.query
+  if (!ids) {
+    return res.status(400).send('Bad Request')
+  }
+  ChildProfile.find({ manage: { $in: ids } })
+    .select('manage')
+    .lean()
+    .exec()
+    .then(child => {
+      child.manage = !child.manage
     }).catch(next)
 })
 
